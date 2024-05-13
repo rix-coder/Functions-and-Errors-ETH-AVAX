e2fa99983a92558373c7d4439e15acb572316980
rix-coder <127965534+rix-coder@users.noreply.github.com>
2024-05-14 01:01:28 +0800

    Create Project1.sol

---
+++ Project1.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract SmartContractProject {
+    function RunRequire (uint a, uint b) public pure {
+        require ( a + b < 20, "Ensure that the sum of the numbers entered is less than 20.");
+    }
+
+    function RunAssert (uint c) public pure {
+        assert ( c >= 5 );
+    }
+
+    function RunRevert (uint d, uint e) public pure {
+        if ( d - e > 10) {
+            revert ("Make sure the difference between the entered numbers is less than 10.");
+        }
+    }
+}
